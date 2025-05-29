be9a60863c58f7eab7fa15ad5c7ac7bf046b099c
ShadowShark99 <kadenwchan@gmail.com>
2025-05-29 14:33:40 -0500

    refactor Project Div creation into ProjectNode

--- src/display.js
+++ src/display.js
@@ -2,60 +2,21 @@ import {Project} from "./project"
 import {Todo} from "./todo"
 //import { SelectedProject} from "./selectedProject"
 import { RootProject } from "./rootProject";
+import { ProjectNode} from "./projectNode";
 
 //aggregation of display functionality
 export const DisplayController = () => {
   
   //return div with project DOM elements
   const projectDisplay = (project) => {
-    const projectDiv = document.createElement('div');
-    const projectTitle = document.createElement('h3');
-    projectTitle.innerHTML = project.getProjectName();
-
-    const projectList = document.createElement('ul');
-    //add todos
-    project.todos.forEach((todo) => {
-      const todoItem = document.createElement('li');
-      todoItem.innerHTML = todo.getTitle() + todo.getDescription() + todo.getDueDate();
-      projectList.appendChild(todoItem);
-    });
-    projectDiv.appendChild(projectTitle);
-    projectDiv.appendChild(projectList);
-
-    project.getProjects().forEach((p) => {
-      projectDiv.appendChild(projectDisplay(p));
-    });
-
-
-    return projectDiv;
+    //folders don't have to be open
+    return ProjectNode(project, false);
   };
 
   //display only open folders
   const projectDisplayOpen = (project) =>
   {
-    const projectDiv = document.createElement('div');
-    const projectTitle = document.createElement('h3');
-    projectTitle.innerHTML = project.getProjectName();
-
-    const projectList = document.createElement('ul');
-    //add todos if the project is open
-    if(project.isOpen())
-      project.todos.forEach((todo) => {
-        const todoItem = document.createElement('li');
-        todoItem.innerHTML = todo.getTitle() + todo.getDescription() + todo.getDueDate();
-        projectList.appendChild(todoItem);
-      });
-    projectDiv.appendChild(projectTitle);
-    projectDiv.appendChild(projectList);
-    
-    //show inner projects if it's open
-    if(project.isOpen())
-      project.getProjects().forEach((p) => {
-        projectDiv.appendChild(projectDisplayOpen(p));
-      });
-
-
-    return projectDiv;
+    return ProjectNode(project, true);
   };
 
   //default display will display folders that are open
---
+++ src/projectNode.js
@@ -0,0 +1,28 @@
+import {Project} from "./project"
+
+//onlyOpen is whether it wants to display only open folders
+export const ProjectNode = (project, onlyOpen) => {
+  const projectDiv = document.createElement('div');
+  const projectTitle = document.createElement('h3');
+  projectTitle.innerHTML = project.getProjectName();
+
+  const projectList = document.createElement('ul');
+  //add todos if the project is open
+  if(!onlyOpen || project.isOpen())
+    project.todos.forEach((todo) => {
+      const todoItem = document.createElement('li');
+      todoItem.innerHTML = todo.getTitle() + todo.getDescription() + todo.getDueDate();
+      projectList.appendChild(todoItem);
+    });
+  projectDiv.appendChild(projectTitle);
+  projectDiv.appendChild(projectList);
+  
+  //show inner projects if it's open
+  if(!onlyOpen || project.isOpen())
+    project.getProjects().forEach((p) => {
+      projectDiv.appendChild(ProjectNode(p, onlyOpen));
+    });
+
+
+  return projectDiv;
+};
