8996f15ce5ce49ca88dd194be0ed0fba4d49a349
ShadowShark99 <kadenwchan@gmail.com>
2025-07-29 20:34:29 -0500

    Add instructions to starting todo

--- src/selectedTodo.js
+++ src/selectedTodo.js
@@ -2,12 +2,9 @@ import { Todo } from "./todo";
 //import { RootProject } from "./rootProject";
 
 const DefaultTodo = () => {
-  return Todo(
-    "Welcome to your Todolist",
-    "Make a new project or add a few todo items to get started",
-    new Date(),
-    0,
-  );
+  const startDesc =
+    "Make a new project or add a few todo items to get started<br><br> <strong><u>Instructions: </u></strong>Click on a project/folder to open/close it. <br>Click on a todo to enlarge details. <br>Click edit on selected project to type new name and press enter to confirm. <br>Delete todos: click on checkbox left of todo -> click on X button of parent project. <br>Delete Project/Folder: Delete all todos within project -> click on X button of parent project and empty project will be deleted. <br>Add Task: adds todo or new project to selected folder/project.";
+  return Todo("Welcome to your Todolist", startDesc, new Date(), 0);
 };
 
 export const SelectedTodo = (() => {
